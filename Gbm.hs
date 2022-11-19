5391595667104a2cd1836d78bba6b557ee67c4de
12gbm <90426799+gbmele@users.noreply.github.com>
2022-11-19 11:17:45 +0000

    not much

--- Gbm.hs
+++ Gbm.hs
@@ -1,15 +1,22 @@
 module Gbm where
-
-mm "s"       = 1 
-mm "A"       = 4
+mm "0"       = 0               --  0 nuffin
+mm "A"       = 1               --  1 am
+mm "AM"      = 1               --  2 pm 
 mm "a"       = 1
+mm "p"       = 2
+mm "P"       = 2
 mm "OTH"     = 0
 mm "AL"      = 4
-mm "AL10"    = 4
+mm "L10"     = 4
 mm "L"       = 4
-mm "U"            = 99
+mm "CL"      = 4
+mm "LPPA"    = 5
+mm "UA"      = 91             -- 91 not AM 
+mm "UP"      = 92             -- 92 not PM 
+mm "U"       = 99
 mm "U00:00-23:59" = 99
 mm "(U:_)"   = 99
+mm "777"     = 777
 mm _         = 0
 
 massert  arg result  =  (== result)(mm arg) 
@@ -19,7 +26,7 @@ mmm x
  | otherwise = 99999
 
 
-leave = ["A","AL","L"]
+leave = ["AL","L","L10","LPPA","CL"]
 
 data Docs =
       ZZZZ
@@ -35,9 +42,43 @@ fE x = fromEnum x
 
 days x y = enumFromTo x y    
 
-doc1 = days 1 7
-doc2 = days 8 14
-doc3 = days 15 21
+di xx = 7
+
+
+doc1 = days  (0 + 1)      (1*(di 9))
+doc2 = days  (1*(di 9)+1) (2*(di 9))
+doc3 = days  (2*(di 9)+1) (3*(di 9))
+doc4 = days  (3*(di 9)+1) (4*(di 9))
+doc5 = days  (4*(di 9)+1) (5*(di 9))
+doc6 = days  (5*(di 9)+1) (6*(di 9))
+doc7 = days  (6*(di 9)+1) (7*(di 9))
+doc8 = days  (7*(di 9)+1) (8*(di 9))
+doc9 = days  (8*(di 9)+1) (9*(di 9))
+
+doc10 = days  (9*(di 9)+1)  (10*(di 9))
+doc11 = days  (10*(di 9)+1) (11*(di 9))
+doc12 = days  (11*(di 9)+1) (12*(di 9))
+doc13 = days  (12*(di 9)+1) (13*(di 9))
+doc14 = days  (13*(di 9)+1) (14*(di 9))
+doc15 = days  (14*(di 9)+1) (15*(di 9))
+doc16 = days  (15*(di 9)+1) (16*(di 9))
+doc17 = days  (16*(di 9)+1) (17*(di 9))
+doc18 = days  (17*(di 9)+1) (18*(di 9))
+doc19 = days  (18*(di 9)+1) (199*(di 9))
+doc20 = days  (19*(di 9)+1) (20*(di 9))
+doc21 = days  (20*(di 9)+1) (21*(di 9))
+doc22 = days  (21*(di 9)+1) (22*(di 9))
+doc23 = days  (22*(di 9)+1) (23*(di 9))
+doc24 = days  (11*(di 9)+1) (12*(di 9))
+doc25 = days  (11*(di 9)+1) (12*(di 9))
+doc26 = days  (11*(di 9)+1) (12*(di 9))
+doc100 = days 1000 1000
+
+-- this array is 0 based - i need to start at 1 index
+docs = [[199..199],doc1,doc2,doc3,doc4,doc5]
+
+get_doc arr doc = arr!!(fromEnum doc)
+
 
 new_row c  = [ 0 | _ <- [1..c]]
 
@@ -68,3 +109,5 @@ gm_CSV fileName = do
     return $ ((map splitcomma) . lines) fileText
 
 rcv array index_size row col = array!!0!!((row-1)*index_size+col-1)
+
+
--- Main.hs
+++ Main.hs
@@ -29,25 +29,37 @@ main = do
 
  print $ map mm [v |(d,v)<-  (zip [1..] (gg!!0)), d `elem` [1..21]]
 
- print $ fromEnum RG
-
  let dump = "requests.dzn"
 
- writeFile dump "ffff\n"
- appendFile dump "and this is a new line\n"
+ writeFile dump "%%ffff\n"
+ appendFile dump "%%and this is a new line\n"
  appendFile dump "array[docs,1..days] of var int: REQUESTS = array2d(docs,1..days,\n"
  appendFile dump (show(map mm [v |(d,v)<-  (zip [1..] (gg!!0)), d `elem` [1..21]]))
 
- appendFile dump "\n ); % end of array dump \n"
+ appendFile dump "\n ); % end of array dump \n\n"
 
  appendFile dump "constraint count([RG,..],l)=" 
- appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` doc1 ,v `elem` leave]))
- appendFile dump ";\n" 
+ appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` (get_doc docs RG) ,v `elem` leave]))
+ appendFile dump ";\n\n" 
 
  appendFile dump  "constraint count([GM,..],l)="
- appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` doc2 ,v `elem` leave]))
- appendFile dump ";\n" 
+ appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` (get_doc docs GM) ,v `elem` leave]))
+ appendFile dump ";\n\n" 
 
  appendFile dump  "constraint count([DB,..],l)="
- appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` doc3 ,v `elem` leave]))
- appendFile dump ";\n" 
+ appendFile dump  (show(length [v | (d,v) <- (zip [1..] (gg!!0)), d `elem` (get_doc docs DB) ,v `elem` leave]))
+ appendFile dump ";\n\n" 
+
+
+
+ appendFile dump "constraint forall(doc in docs, day in 1..days)\n"
+ appendFile dump "  (if     REQUESTS[ doc, day ] = 4  then roster[ doc, day ]   =  l \n"
+ appendFile dump "   elseif REQUESTS[ doc, day ] = 99 then roster[ doc, day ]   =  o \n"
+ appendFile dump "   elseif REQUESTS[ doc, day ] = 1  then roster[ doc, day ]   =  a \n"
+ appendFile dump "   elseif REQUESTS[ doc, day ] = 91 then roster[ doc, day ]  !=  a \n"
+ appendFile dump "   elseif REQUESTS[ doc, day ] = 2  then roster[ doc, day ]   =  p \n"
+ appendFile dump "   elseif REQUESTS[ doc, day ] = 92 then roster[ doc, day ]  !=  p \n"
+ appendFile dump "   else true\n"
+ appendFile dump "   endif);"
+
+ print $ docs
